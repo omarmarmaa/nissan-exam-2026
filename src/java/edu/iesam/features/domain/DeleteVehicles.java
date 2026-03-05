a7674328b8da861d721685ed10ff185f53983438
MMO <omarmarouf345@gmail.com>
2026-03-05 19:22:56 +0100

    Aqui procedemos a insertar los casos de uso pedidos

---
+++ src/java/edu/iesam/features/domain/DeleteVehicles.java
@@ -0,0 +1,8 @@
+package java.edu.iesam.features.domain;
+
+public class DeleteVehicles {
+    public static void delete(){
+       
+    }
+
+}
---
+++ src/java/edu/iesam/features/domain/SaveVehicles.java
@@ -0,0 +1,8 @@
+package java.edu.iesam.features.domain;
+
+public class SaveVehicles {
+
+    public static void save(){
+        Vehicle vehicle=new Vehicle("coche","123445","2006","1");
+    }
+}
